75486e9fd50e1df194e73bab39de5b3af375be0a
kavita <kavitaraghvani09@gmail.com>
2026-05-26 15:49:42 +0530

    update&delete

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -19,3 +19,39 @@ exports.getBooks = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+// getSingleBook
+exports.getBook = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status;
+      (404).json({ message: "Book not found!" });
+    }
+    res.status(200).json({ success: "true", data: book });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+//updateBook
+exports.updateBook = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({ success: "true", data: book });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// deleteBook
+exports.deleteBook = async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.status(200).json({ success: "true", message: "Book Deleted!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createBooks, getBooks } = require("../controllers/bookController");
+const {
+  createBooks,
+  getBooks,
+  getBook,
+  updateBook,
+  deleteBook,
+} = require("../controllers/bookController");
 
 router.post("/", createBooks);
 router.get("/books", getBooks);
+router.get("/:id", getBook);
+router.put("/:id", updateBook);
+router.delete("/:id", deleteBook);
 
 module.exports = router;
